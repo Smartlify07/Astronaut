0bc70a6bb016be7e776b916791bd39249b90a8f9
Obinna Anosike <smartlify09@gmail.com>
2024-11-29 23:33:09 +0100

    built

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('main');
+const mainBg = document.querySelector('main').style.backgroundImage;
+
+const nextBtn = document.querySelector('.next');
+nextBtn.addEventListener('click', () => {
+  setTimeout(() => {}, 2000);
+  //   main.style.backgroundImage = 'url(../images/astro2.webp)';
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: 'Inter, sans-serif',
+        prompt: 'Prompt, sans-serif',
+      },
+    },
+  },
+  plugins: [],
+};
